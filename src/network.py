4499b88e897b5cc896cb282ee1bfaaac214d9dcb
sine_x <zhangzhenghao@hotmail.com>
2018-02-19 16:10:08 +0800

    Add batch norm & reduce actions

--- src/network.py
+++ src/network.py
@@ -1,5 +1,6 @@
-import cv2
 import math
+
+import cv2
 import numpy as np
 import tensorflow as tf
 
@@ -14,23 +15,23 @@ class PilotNet:
 
     def __init__(self, learning_rate=0.1):
         """
-        Create a PilotNet
+        Create a PilotNet.
         :param learning_rate: learning rate for Adam optimizer
         """
         # Placeholders
         self.input_image = tf.placeholder(tf.float32, [None, self.INPUT_HEIGHT, self.INPUT_WIDTH, self.INPUT_CHANNEL])
         self.input_label = tf.placeholder(tf.int64, [None])
         # Convolution neural network
-        conv1 = tf.layers.conv2d(self.input_image, 24, 5, 2, activation=tf.nn.relu)
-        conv2 = tf.layers.conv2d(conv1, 36, 5, 2, activation=tf.nn.relu)
-        conv3 = tf.layers.conv2d(conv2, 48, 5, 2, activation=tf.nn.relu)
-        conv4 = tf.layers.conv2d(conv3, 64, 3, activation=tf.nn.relu)
-        conv5 = tf.layers.conv2d(conv4, 64, 3, activation=tf.nn.relu)
+        conv1 = self.conv2d_norm_relu(self.input_image, 24, 5, 2)
+        conv2 = self.conv2d_norm_relu(conv1, 36, 5, 2)
+        conv3 = self.conv2d_norm_relu(conv2, 48, 5, 2)
+        conv4 = self.conv2d_norm_relu(conv3, 64, 3)
+        conv5 = self.conv2d_norm_relu(conv4, 64, 3)
         flat = tf.layers.flatten(conv5)
         fc1 = tf.layers.dense(flat, 1164, activation=tf.nn.relu)
         dropout = tf.layers.dropout(fc1, 0.5)
         fc2 = tf.layers.dense(dropout, 100, activation=tf.nn.relu)
-        logits = tf.layers.dense(fc2, 3)
+        logits = tf.layers.dense(fc2, 2)
         self.output_softmax = tf.nn.softmax(logits)
         self.output_label = tf.argmax(self.output_softmax, 1)
         self.output_acc = tf.reduce_mean(tf.cast(tf.equal(self.output_label, self.input_label), tf.float32))
@@ -46,8 +47,8 @@ class PilotNet:
         feature_map1 = tf.reduce_mean(conv1, 3, keep_dims=True) * feature_map2_scaled
         self.output_masks = tf.layers.conv2d_transpose(feature_map1, 1, 5, 2, kernel_initializer=tf.ones_initializer(), trainable=False)
         # Loss and train step
-        self.loss = tf.losses.softmax_cross_entropy(tf.one_hot(self.input_label, 3), logits)
-        optimizer = tf.train.AdadeltaOptimizer(learning_rate)
+        self.loss = tf.losses.softmax_cross_entropy(tf.one_hot(self.input_label, 2), logits)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
         self.train_step = optimizer.minimize(self.loss)
         # Create session
         self.sess = tf.Session()
@@ -130,7 +131,7 @@ class PilotNet:
 
     def save(self, filename):
         """
-        Save parameters to files
+        Save parameters to files.
         :param filename: file name
         """
         saver = tf.train.Saver()
@@ -138,24 +139,37 @@ class PilotNet:
 
     def load(self, filename):
         """
-        Load parameters from files
+        Load parameters from files.
         :param filename: file name
         """
         saver = tf.train.Saver()
         saver.restore(self.sess, filename)
+
+    @staticmethod
+    def conv2d_norm_relu(inputs, filters, kernel_size, stride=1):
+        """
+        The procedure of "convolution -> batch normalization -> relu".
+        :param inputs: the input tensor
+        :param filters: the number of filters
+        :param kernel_size: the size of convolution kernel
+        :param stride: the stride of convolution
+        :return: the output tensor
+        """
+        conv = tf.layers.conv2d(inputs, filters, kernel_size, stride)
+        bn = tf.layers.batch_normalization(conv, 3)
+        return tf.nn.relu(bn)
 
 
 if __name__ == "__main__":
     # Load 'data'
     L = cv2.imread('test/L.png')
     R = cv2.imread('test/R.png')
-    U = cv2.imread('test/U.png')
-    train_image = np.stack([L, R, U] * 10, 0)
-    train_label = np.asarray([0, 1, 2] * 10)
-    val_image = np.stack([L, R, U] * 5, 0)
-    val_label = np.asarray([0, 1, 2] * 5)
+    train_image = np.stack([L, R] * 10)
+    train_label = np.asarray([0, 1] * 10)
+    val_image = np.stack([L, R] * 5)
+    val_label = np.asarray([0, 1] * 5)
     # Create model
-    net = PilotNet()
+    net = PilotNet(1e-6)
     # Train model
     net.fit(train_image, train_label, val_image, val_label, epoch=1, iters=100, print_iters=10)
     # Save model
@@ -163,7 +177,4 @@ if __name__ == "__main__":
     # Load model
     net.load('test/test.ckpt')
     # Predict
-    pred, mask = net.predict(np.asarray([L]))
-    cv2.imshow('%s' % np.argmax(pred), mask[0])
-    cv2.waitKey()
-
+    pred, salient = net.predict(np.asarray([L]))
