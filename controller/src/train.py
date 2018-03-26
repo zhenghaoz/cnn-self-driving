bbab33e88cc730a0921eadbc1fb1e7ac16987159
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2018-03-26 14:25:01 +0800

    Rename directories

---
+++ controller/src/train.py
@@ -0,0 +1,10 @@
+import config
+from loader import DataLoader
+from network import PilotNet
+
+if __name__ == '__main__':
+    loader = DataLoader(config.DIR_DATA, mirror=True)
+    net = PilotNet(1e-4)
+    net.load('model/driver.ckpt')
+    net.fit(loader.train_images, loader.train_labels, loader.test_images, loader.test_labels, print_iters=10, batch_size=200, epoch=1)
+    net.save('model/driver.ckpt')
