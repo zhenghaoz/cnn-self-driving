d80fa9c3ad740cce77b3ce1b52a6089425e291fe
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2018-03-29 13:47:16 +0800

    Implemented scaled content

--- controller/src/form.py
+++ controller/src/form.py
@@ -1,14 +1,28 @@
 import json
 
-from PyQt5 import Qt
+from PyQt5 import Qt, QtGui
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 
+class QFlexLabel(QLabel):
+
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(1,1)
+
+    def setPixmap(self, a0: QtGui.QPixmap):
+        super().setPixmap(a0.scaled(self.size(), Qt.KeepAspectRatio))
+
+    def resizeEvent(self, a0: QtGui.QResizeEvent):
+        if self.pixmap():
+            super().setPixmap(self.pixmap().scaled(self.size(), Qt.KeepAspectRatio))
+
+
 class ContentForm(QMainWindow):
 
-    def __init__(self, layout_file):
+    def __init__(self, layout_file: str):
         super().__init__()
         # Load configuration
         with open(layout_file, 'r') as file:
@@ -32,7 +46,10 @@ class ContentForm(QMainWindow):
             menu_item = menu_bar.addMenu(item_layout["title"])
             if "action" in item_layout.keys():
                 for action in item_layout["action"]:
-                    menu_item.addAction(self.action_set[action])
+                    if action == "-":
+                        menu_item.addSeparator()
+                    else:
+                        menu_item.addAction(self.action_set[action])
         # Setup toolbar
         toolbar_layout = layout["toolbar"]
         for item_layout in toolbar_layout:
@@ -43,11 +60,13 @@ class ContentForm(QMainWindow):
                 for action in item_layout["action"]:
                     toobar_item.addAction(self.action_set[action])
         # Setup status bar
+        self.statusbar_set = {}
         statusbar_layout = layout["status"]
         for item_layout in statusbar_layout:
             statusbar_item = QLabel()
             statusbar_item.setText(item_layout["text"])
             self.statusBar().addPermanentWidget(statusbar_item, item_layout["stretch"])
+            self.statusbar_set[item_layout["title"]] = statusbar_item
         # Setup form
         self.setWindowTitle(layout["title"])
         self.setWindowIcon(QIcon(layout["icon"]))
@@ -57,12 +76,18 @@ class ContentForm(QMainWindow):
             self.move((screen_size.width() / 2) - (frame_size.width() / 2),
                       (screen_size.height() / 2) - (frame_size.height() / 2))
         # Setup content
-        self.content_label = QLabel()
+        self.content_label = QFlexLabel()
         self.setCentralWidget(self.content_label)
         self.content_label.setStyleSheet('background-color: ' + layout["background-color"])
         self.content_label.setAlignment(Qt.AlignCenter)
-        self.set_content(QPixmap(layout["default-content"]))
+        self.setContent(QPixmap(layout["default-content"]))
 
-    def set_content(self, content):
+    def setContent(self, content: QPixmap):
         self.content = content
-        self.content_label.setPixmap(self.content.scaled(self.content_label.size(), Qt.KeepAspectRatio))
+        self.content_label.setPixmap(self.content)
+
+    def setEvent(self, action, func):
+        self.action_set[action].triggered.connect(func)
+
+    def setText(self, statusbar, text):
+        self.statusbar_set[statusbar].setText(text)
