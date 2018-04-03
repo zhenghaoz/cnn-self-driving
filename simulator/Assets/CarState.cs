9ac2d8bf4967857a058d847d84059555e3218cf4
ZhangZhenghao <zhangzhenghao@hotmail.com>
2018-04-03 19:31:55 +0800

    Add car simulator
    Use virtual env from https://github.com/timediv/reinforced-race

---
+++ simulator/Assets/CarState.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarState : MonoBehaviour {
+
+	private class TransformState {
+		public Vector3 position;
+		public Quaternion rotation;
+		
+		public TransformState(Vector3 position, Quaternion rotation) {
+			this.position = position;
+			this.rotation = rotation;
+		}
+	}
+
+	public bool Disqualified { get; private set; }
+	public bool Finished { get; private set; }
+
+	public uint maxTransformHistoryLength = 100;
+	public float transformHistorySaveDistance = 1f;
+	public float resetMinDistance = 5f;
+	public float resetMaxDistance = 12f;
+
+	private GameObject[] checkpoints;
+	private int checkpointsPassed = 0;
+
+	private LinkedList<TransformState> transformHistory;
+
+	private Vector3 lastPosition;
+	private float distanceDriven = 0f;
+	private float lastHistoryTransformDistance = 0f;
+
+	void Start () {
+		checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+
+		TransformState startState = new TransformState(transform.position, transform.rotation);
+		InitializeTransformHistory(startState);
+
+		lastPosition = transform.position;
+	}
+
+	private void InitializeTransformHistory(TransformState state) {
+		transformHistory = new LinkedList<TransformState>();
+		transformHistory.AddLast(state);
+	}
+
+	public void ResetState() {
+		Disqualified = false;
+		Finished = false;
+	}
+
+	private void SaveTransform() {
+		if(Disqualified)
+			return;
+		TransformState state = new TransformState(transform.position, transform.rotation);
+		transformHistory.AddLast(state);
+		if(transformHistory.Count > maxTransformHistoryLength)
+			transformHistory.RemoveFirst();
+		lastHistoryTransformDistance = distanceDriven;
+	}
+
+	void Update() {
+		distanceDriven += (transform.position - lastPosition).magnitude;
+		lastPosition = transform.position;
+		if(distanceDriven - lastHistoryTransformDistance > transformHistorySaveDistance)
+			SaveTransform();
+	}
+
+	private TransformState SampleHistoryState() {
+		int earliestPermittedSample = System.Math.Max(0, transformHistory.Count -
+										(int)(resetMaxDistance / transformHistorySaveDistance));
+		int latestPermittedSample = System.Math.Max(1, transformHistory.Count -
+										(int)(resetMinDistance / transformHistorySaveDistance));
+		int historySample = Random.Range(earliestPermittedSample, latestPermittedSample);
+
+		// Delete all following history states
+		while(transformHistory.Count - 1 != historySample)
+			transformHistory.RemoveLast();
+		
+		return transformHistory.Last.Value;
+	}
+
+	private void ResetVehicle() {
+		CarController carController = GetComponent<CarController>();
+		Rigidbody body = GetComponent<Rigidbody>();
+		TransformState historicalState = SampleHistoryState();
+
+		carController.ApplyAction(0f, 0f);
+		transform.position = historicalState.position;
+		transform.rotation = historicalState.rotation;
+		body.velocity = Vector3.zero;
+		body.angularVelocity = Vector3.zero;
+		carController.ApplyAction(1f, 0f);
+	}
+
+	private void ResetCheckpoints() {
+		checkpointsPassed = 0;
+		foreach(GameObject checkpoint in checkpoints)
+			checkpoint.SetActive(true);
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		switch(other.tag) {
+		case "Disqualification":
+			Disqualified = true;
+			ResetCheckpoints();
+			ResetVehicle();
+			Debug.Log("Car was disqualified");
+			break;
+		case "Checkpoint":
+			other.gameObject.SetActive(false);
+			checkpointsPassed++;
+			Debug.Log("Car passed checkpoint");
+			break;
+		case "FinishLine":
+			if(checkpointsPassed == checkpoints.Length) {
+				Finished = true;
+				ResetCheckpoints();
+				Debug.Log("Car passed finish line");
+			}
+			break;
+		}
+	}
+}
---
+++ simulator/Assets/EnvironmentCommunicator.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System;
+using System.IO;
+
+public class EnvironmentCommunicator : MonoBehaviour {
+
+	public int port = 2851;
+	public const int maxImageSize = 256;
+
+	private CameraSensor cameraSensor;
+	private CarController carController;
+	private CarState carState;
+
+	private Socket listener;
+	private Client client;
+
+	private const byte REQUEST_READ_SENSORS = 1;
+	private const byte REQUEST_WRITE_ACTION = 2;
+
+	private class Client {
+
+		public Client(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void BeginReceive(AsyncCallback callback) {
+			socket.BeginReceive(buffer, 0, BufferSize, 0, callback, this);
+		}
+
+		public Socket socket;
+		public const int BufferSize = 1024;
+		public int bytesLeft = 0;
+		public byte[] buffer = new byte[BufferSize];
+		public volatile bool requestPending;
+		public const int ResponseBufferSize = maxImageSize * maxImageSize + 16;
+		public byte[] responseBuffer = new byte[ResponseBufferSize];
+	}
+
+	private int DeterminePort() {
+		string[] args = System.Environment.GetCommandLineArgs();
+		for(int i = 0; i < args.Length; i++) {
+			if(args[i] == "--port" && i + 1 < args.Length)
+				return Convert.ToInt32(args[i + 1]);
+		}
+		// or return default port
+		return port;
+	}
+
+	void Start() {
+		cameraSensor = GetComponentInChildren<CameraSensor>();
+		carState = GetComponent<CarState>();
+		carController = GetComponent<CarController>();
+
+		IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, DeterminePort());
+
+		listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		try {
+			listener.Bind(localEndPoint);
+			listener.Listen(1);
+
+			BeginAccept();
+		} catch (Exception e) {
+			Debug.LogException(e);
+		}
+	}
+
+	private void BeginAccept() {
+		listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+	}
+
+	private void AcceptCallback(IAsyncResult ar) {
+		Socket listener = (Socket) ar.AsyncState;
+		Socket handler = listener.EndAccept(ar);
+
+		Debug.Log("Accepted new client");
+
+		client = new Client(handler);
+		client.BeginReceive(new AsyncCallback(ReadCallback));
+	}
+
+	private void ReadCallback(IAsyncResult ar) {  
+		int bytesRead = client.socket.EndReceive(ar);  
+
+		if (bytesRead > 0) {
+			client.bytesLeft = bytesRead;
+			client.requestPending = true;
+		}
+	}
+
+	private void SendSensorData(int imageWidth, int imageHeight) {
+		imageWidth = Math.Min(imageWidth, maxImageSize);
+		imageHeight = Math.Min(imageHeight, maxImageSize);
+		cameraSensor.TakePicture(image => {
+			int responseSize = 6 + imageWidth * imageHeight;
+			BinaryWriter writer = new BinaryWriter(new MemoryStream(client.responseBuffer));
+			writer.Write(carState.Disqualified);
+			writer.Write(carState.Finished);
+			// We can't transfer floating point values, so let's transmit velocity * 2^16
+			int velocity = IPAddress.HostToNetworkOrder((int)(carController.GetVelocity() * 0xffff));
+			writer.Write(velocity);
+			carState.ResetState();
+			WriteCameraImage(writer, image);
+			client.socket.BeginSend(client.responseBuffer, 0, responseSize, 0, new AsyncCallback(SendCallback), client);
+		}, imageWidth, imageHeight);
+	}
+
+	private void ApplyAction(int vertical, int horizontal) {
+		carController.ApplyAction(vertical, horizontal);
+	}
+
+	void Update() {
+		if(client != null && client.requestPending) {
+			client.requestPending = false;
+			BinaryReader reader = new BinaryReader(new MemoryStream(client.buffer));
+			while(client.bytesLeft > 0) {
+				byte instruction = reader.ReadByte();
+				client.bytesLeft--;
+				switch(instruction) {
+				case REQUEST_READ_SENSORS:
+					int width = IPAddress.NetworkToHostOrder(reader.ReadInt32());
+					int height = IPAddress.NetworkToHostOrder(reader.ReadInt32());
+					client.bytesLeft -= 8;
+					SendSensorData(width, height);
+					break;
+				case REQUEST_WRITE_ACTION:
+					int vertical = IPAddress.NetworkToHostOrder(reader.ReadInt32());
+					int horizontal = IPAddress.NetworkToHostOrder(reader.ReadInt32());
+					client.bytesLeft -= 8;
+					ApplyAction(vertical, horizontal);
+					break;
+				}
+			}
+			// Listen for next message
+			client.BeginReceive(new AsyncCallback(ReadCallback));
+		}
+	}
+
+	private void SendCallback(IAsyncResult ar) {
+		client.socket.EndSend(ar);
+	}  
+
+	private void WriteCameraImage(BinaryWriter writer, Texture2D image) {
+		for(int y = 0; y < image.height; y++) {
+			for(int x = 0; x < image.width; x++) {
+				Color pixel = image.GetPixel(x, y);
+				byte grayscale = (byte)(pixel.grayscale * 255);
+				writer.Write(grayscale);
+			}
+		}
+	}
+}
