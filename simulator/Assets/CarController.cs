3f57b646cd081958926da3de8e60508dacfcac6c
ZhangZhenghao <zhangzhenghao@hotmail.com>
2018-04-04 14:01:20 +0800

    Implemented controller

--- simulator/Assets/CarController.cs
+++ simulator/Assets/CarController.cs
@@ -1,79 +1,112 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
 public class CarController : MonoBehaviour {
-	
-	public List<AxleInfo> axleInfos; // the information about each individual axle
-	public float maxMotorTorque; // maximum torque the motor can apply to wheel
-	public float maxSteeringAngle; // maximum steer angle the wheel can have
 
-	private bool appliedManualAction = false;
-	private Rigidbody body;
+	public float transportSpeed;
+	public float rotateSpeed;
 
-	// finds the corresponding visual wheel
-	// correctly applies the transform
-	public void ApplyLocalPositionToVisuals(WheelCollider collider)
-	{
-		if(collider.transform.childCount == 0) {
-			return;
-		}
+	private enum Action { Stop, Forward, Backward, TurnLeft, TurnRight }
 
-		Transform visualWheel = collider.transform.GetChild(0);
-
-		Vector3 position;
-		Quaternion rotation;
-		collider.GetWorldPose(out position, out rotation);
-
-		visualWheel.transform.position = position;
-		visualWheel.transform.rotation = rotation;
-	}
-
-	public void FixedUpdate() {
-		float vertical = Input.GetAxis("Vertical");
-		float horizontal = Input.GetAxis("Horizontal");
-
-		// Override autonomous behaviour
-		if(vertical != 0f || horizontal != 0f || appliedManualAction) {
-			ApplyAction(vertical, horizontal);
-			appliedManualAction = true;
-		}
-	}
+	private Action action;
+	private Stack<KeyCode> keyStack = new Stack<KeyCode>();
+	private HashSet<KeyCode> keyPressed = new HashSet<KeyCode>();
 
 	public void Start() {
-		body = GetComponent<Rigidbody>();
+		keyStack.Push (KeyCode.Space);
 	}
 
-	public float GetVelocity() {
-		return (transform.worldToLocalMatrix * body.velocity).z;
-	}
-
-	public void ApplyAction(float vertical, float horizontal)
-	{
-		float motor = maxMotorTorque * vertical;
-		float steering = maxSteeringAngle * horizontal;
-
-		foreach(AxleInfo axleInfo in axleInfos) {
-			if(axleInfo.steering) {
-				axleInfo.leftWheel.steerAngle = steering;
-				axleInfo.rightWheel.steerAngle = steering;
-			}
-			if(axleInfo.motor) {
-				axleInfo.leftWheel.motorTorque = motor;
-				axleInfo.rightWheel.motorTorque = motor;
-			}
-			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
-			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+	public void Update() {
+		// Get input
+		if (Input.GetKeyDown(KeyCode.W))
+			KeyDown (KeyCode.W);
+		if (Input.GetKeyDown(KeyCode.A))
+			KeyDown (KeyCode.A);
+		if (Input.GetKeyDown(KeyCode.S))
+			KeyDown (KeyCode.S);
+		if (Input.GetKeyDown(KeyCode.D))
+			KeyDown (KeyCode.D);
+		if (Input.GetKeyUp(KeyCode.W))
+			KeyUp (KeyCode.W);
+		if (Input.GetKeyUp(KeyCode.A))
+			KeyUp (KeyCode.A);
+		if (Input.GetKeyUp(KeyCode.S))
+			KeyUp (KeyCode.S);
+		if (Input.GetKeyUp(KeyCode.D))
+			KeyUp (KeyCode.D);
+		// Apply action
+		switch(action) {
+		case Action.Forward:
+			transform.position += transform.forward * transportSpeed * Time.deltaTime;
+			break;
+		case Action.Backward:
+			transform.position += transform.forward * -transportSpeed * Time.deltaTime;
+			break;
+		case Action.TurnLeft:
+			transform.Rotate(Vector3.up * -rotateSpeed * Time.deltaTime, Space.World);
+			break;
+		case Action.TurnRight:
+			transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+			break;
 		}
-
-		appliedManualAction = false;
 	}
-}
 
-[System.Serializable]
-public class AxleInfo {
-	public WheelCollider leftWheel;
-	public WheelCollider rightWheel;
-	public bool motor; // is this wheel attached to motor?
-	public bool steering; // does this wheel apply steer angle?
+	private void KeyDown(KeyCode keyCode) {
+		if (!keyPressed.Contains (keyCode)) 
+			KeyApply (keyCode);
+			keyPressed.Add (keyCode);
+			keyStack.Push (keyCode);
+	}
+
+	private void KeyUp(KeyCode keyCode) {
+		if (keyPressed.Contains (keyCode))
+			keyPressed.Remove (keyCode);
+		while (keyStack.Count > 1) {
+			if (!keyPressed.Contains (keyStack.Peek ()))
+				keyStack.Pop ();
+			else
+				break;
+		}
+		KeyApply (keyStack.Peek ());
+	}
+
+	private void KeyApply(KeyCode keyCode) {
+		switch (keyCode) {
+		case KeyCode.Space:
+			Stop ();
+			break;
+		case KeyCode.W:
+			Forward ();
+			break;
+		case KeyCode.S:
+			Backward ();
+			break;
+		case KeyCode.A:
+			TurnLeft ();
+			break;
+		case KeyCode.D:
+			TurnRight ();
+			break;
+		}
+	}
+		
+	public void Stop() {
+		action = Action.Stop;
+	}
+
+	public void Forward() {
+		action = Action.Forward;
+	}
+
+	public void Backward() {
+		action = Action.Backward;
+	}
+
+	public void TurnLeft() {
+		action = Action.TurnLeft;
+	}
+
+	public void TurnRight() {
+		action = Action.TurnRight;
+	}
 }
