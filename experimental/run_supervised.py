7ae30431b23afcf2a69176cbeee55b8dc7b767d0
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2018-06-24 18:53:16 +0800

    Remove reinforcement learning content

--- experimental/run_supervised.py
+++ experimental/run_supervised.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-import sys
 
 import pickle
+
 import numpy as np
 import tensorflow as tf
-from dataset import DataFile
 
 from env import Env
 from net import build_cnn, build_mlp
@@ -28,6 +27,7 @@ class NeuralNetwork:
             self.sy_logit = build_cnn(self.sy_obs, num_actions, "cnn")
         else:
             raise ValueError("Unsupported observation dimension " + obs_dim)
+        self.sy_softmax = tf.nn.softmax(self.sy_logit, 1)
         self.sy_out = tf.argmax(self.sy_logit, 1)
         # Train step
         self.sy_loss = tf.losses.softmax_cross_entropy(tf.one_hot(self.sy_act, num_actions), self.sy_logit, reduction=tf.losses.Reduction.MEAN)
@@ -70,9 +70,13 @@ class NeuralNetwork:
         :param observations: observations for predicting
         :return: actions
         """
-        return self.sess.run(self.sy_out, {
+        return self.sess.run([self.sy_out, self.sy_softmax], {
             self.sy_obs: observations
         })
+
+    def load(self, filename):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, filename)
 
     def save(self, filename):
         """
@@ -100,7 +104,9 @@ def main():
     parser.add_argument('--num_rollouts', type=int, default=20, help='Number of expert roll outs')
     parser.add_argument('--batch_size', type=int, default=100)
     parser.add_argument('--n_iter', type=int, default=1000)
+    parser.add_argument('--pre_train', '-p', type=str)
     parser.add_argument('--output', '-o', type=str)
+    parser.add_argument('--shake_avoid', '-sa', action='store_true')
     args = parser.parse_args()
 
     # Create environment
@@ -108,15 +114,23 @@ def main():
     env = Env('Image')
 
     # Setup data set
-    observations, actions = read_data('dataset_simulator.dat')
+    observations, actions = read_data('dataset_sim.dat')
 
     # Create neural network
     net = NeuralNetwork(env.observation_space.shape, env.action_space.n)
 
+    if args.pre_train:
+        net.load(args.pre_train)
+
     # Fit expert data
     net.fit(observations, actions, args.n_iter, args.batch_size)
 
+    # Save model
+    if args.output:
+        net.save(args.output)
+
     returns_policy = []
+    actions = []
     env.connect()
     for i in range(args.num_rollouts):
         obs, info = env.reset(with_info=True)
@@ -124,7 +138,11 @@ def main():
         total_reward = 0.
         steps = 0
         while not done:
-            action_policy = net.predict([obs])[0]
+            a, b = net.predict([obs])
+            action_policy = a[0]
+            if args.shake_avoid and len(actions) >= 2 and actions[-1] + actions[-2] == 1:
+                action_policy = 2
+            actions.append(action_policy)
             obs, r, done, info = env.step(action_policy)
             total_reward += r
             steps += 1
@@ -138,10 +156,6 @@ def main():
     print('returns', returns_policy)
     print('mean return', np.mean(returns_policy))
     print('std of return', np.std(returns_policy))
-
-    # Save model
-    if args.output:
-        net.save(args.output)
 
 
 if __name__ == '__main__':
